22c9f8a4eeda86da25aedaab2d0b3ba14d6e415a
Gal Habjan <galhabjan100@gmail.com>
2025-10-12 21:00:26 +0200

    basic layout

---
+++ file-explorers-fe/src/components/topBar/Bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="top_bar"></div>
+
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.top_bar {
+  background: #045908;
+  width: 100%;
+  height: 100%;
+}
+</style>
