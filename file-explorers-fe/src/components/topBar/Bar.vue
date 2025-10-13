dbad5b3c28ca2bb38cfdd96a52ce415395a54711
Gal Habjan <galhabjan100@gmail.com>
2025-10-13 23:56:56 +0200

    prime vue

--- file-explorers-fe/src/components/topBar/Bar.vue
+++ file-explorers-fe/src/components/topBar/Bar.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="top_bar"></div>
+  <div class="top_bar">
+    <Textarea v-model="value" rows="2" fluid />
+  </div>
 
 </template>
 
 
 <script lang="ts" setup>
-
+import Textarea from 'primevue/textarea';
+import { ref } from 'vue';
+const value = ref<string>("C:");
 </script>
 
 <style lang="scss" scoped>
@@ -13,5 +17,10 @@
   background: #045908;
   width: 100%;
   height: 100%;
+}
+
+Button {
+  height: 100%;
+  width: 100%;
 }
 </style>
