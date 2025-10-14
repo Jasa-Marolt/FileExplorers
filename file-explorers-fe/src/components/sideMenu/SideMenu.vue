75de540cf382126ddf688bd7285b8693658eb70c
Gal Habjan <galhabjan100@gmail.com>
2025-10-14 20:04:08 +0200

    side menu and top bar
    added prime vue components and setup layout based on patriks design

--- file-explorers-fe/src/components/sideMenu/SideMenu.vue
+++ file-explorers-fe/src/components/sideMenu/SideMenu.vue
@@ -1,17 +1,47 @@
 <template>
-    <div class="side_menu"></div>
+    <div class="side_menu">
+        <div class="menu-group">
+            <Button type="button" label="Level_1" icon="pi pi-folder" />
+            <Button type="button" label="Level_2" icon="pi pi-folder" />
+            <Button type="button" label="Level_3" icon="pi pi-folder" />
+        </div>
 
+        <Divider />
+
+        <div class="menu-group">
+            <Button type="button" label="Level_1" icon="pi pi-folder" />
+            <Button type="button" label="Level_2" icon="pi pi-folder" />
+            <Button type="button" label="Level_3" icon="pi pi-folder" />
+        </div>
+    </div>
 </template>
 
 
 <script lang="ts" setup>
-
+import Button from 'primevue/button';
+import Divider from 'primevue/divider';
 </script>
 
 <style lang="scss" scoped>
 .side_menu {
-    background: rgb(46, 96, 48);
-    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background: var(--background-secondary);
+    border-right: 3px solid var(--border-color);
+    min-width: 13vw;
     height: 100%;
+    padding: 0.5rem;
+    gap: 0.5rem;
+}
+
+.menu-group {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    /* spacing only between buttons */
+}
+
+::v-deep(.p-divider) {
+    margin: 0.25rem 0;
 }
 </style>
